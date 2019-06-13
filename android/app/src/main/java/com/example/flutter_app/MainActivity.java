e5d2c0974e1528c090a0e829dca33a13734cbe87
lvqiu <1352101891@qq.com>
2019-06-14 00:16:02 +0800

    flutter练习项目

---
+++ android/app/src/main/java/com/example/flutter_app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.flutter_app;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+    HashMap map=new HashMap();
+
+  }
+}
---
+++ lib/shopcart.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:event_bus/event_bus.dart';
+import 'package:flutter_app/main.dart';
+
+class Production{
+  String price;
+  String name;
+  Production({this.price, this.name});
+}
+
+class util{
+  //私有方法不可复写
+  Color _getColor(BuildContext context,bool inCart) {
+    return inCart ? Theme.of(context).primaryColor:Colors.black54 ;
+  }
+
+  //私有方法不可复写
+  TextStyle _getTextStyle(BuildContext context,bool inCart) {
+    if (!inCart) return null;
+
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+}
+
+typedef void ClickShopItem(bool isAdd,Production prod);
+
+class ShopItem extends StatelessWidget with util{
+  final bool inCart;
+  final Production p;
+  final ClickShopItem clickShopItem;
+
+  ShopItem(this.inCart, Production prod,this.clickShopItem):p = prod,
+        super(key: new ObjectKey(prod));
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        clickShopItem(!inCart,p);
+      },
+      leading: new CircleAvatar(
+        backgroundColor: _getColor(context,inCart),
+        child: Icon(Icons.ac_unit),
+      ),
+      trailing: new Icon(inCart?Icons.check:Icons.add),
+      title: new ItemLayout(p,inCart),
+    );
+  }
+}
+
+class ItemLayout extends StatelessWidget with util{
+  bool inCart;
+  Production p;
+  ItemLayout(this.p,this.inCart);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        children:<Widget>[
+          Text("名称:"+p.name, style: inCart?Theme.of(context).textTheme.title:Theme.of(context).textTheme.subtitle),
+          Text("价格:"+p.price, style: _getTextStyle(context,inCart)),
+        ]
+    );
+  }
+}
+
+class MyListView extends StatefulWidget {
+  List<Production> productions=new List();
+
+  MyListView({Key key,this.productions}) : super(key: key);
+
+  @override
+  ShopList createState() => ShopList();
+}
+
+class ShopList extends State<MyListView> {
+  Set<Production> _shoppingCart=new Set();
+  EventBus eventBus = new EventBus();
+
+  @override
+  void initState() {
+    super.initState();
+    eventBus.on<ClearAllEvent>().listen((event) {
+      print(event.flag);
+      if(event.flag){
+        _clear();
+      }
+    });
+  }
+
+  void _clear(){
+    setState(() {
+      _shoppingCart.clear();
+    });
+  }
+
+  void _add(Production p) {
+    setState(() {
+       _shoppingCart.add(p);
+    });
+  }
+
+  void _remove(Production p){
+    setState(() {
+      _shoppingCart.remove(p);
+    });
+  }
+
+  void _clickItem(bool inCart,Production p){
+    if(inCart) {
+      _add(p);
+    }else{
+      _remove(p);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.productions.map((p)=>
+            ShopItem(_shoppingCart.contains(p),p,_clickItem)
+        ).toList(),
+      )
+    );
+  }
+}
